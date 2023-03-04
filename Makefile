1620adf858c98b043ae55184ab27fefd2745c510
DFlo <dwflorek@yahoo.com>
2023-03-04 01:11:26 -0800

    add initial GLFW support

--- Makefile
+++ Makefile
@@ -1,14 +1,16 @@
 
+GLFW_HOME=/usr/local/glfw
+
 all: tutorial
 
 tutorial: tutorial.o
-	g++ $^ -o $@
+	g++ $^ -o $@ -L$(GLFW_HOME)/lib -lglfw
 
 tutorial.so: tutorial.o
 	gcc $^ -shared -o $@
 
 .cpp.o:
-	g++ -c -g $<
+	g++ -c -g -I$(GLFW_HOME)/include $<
 
 clean:
 	rm -f tutorial tutorial.so tutorial.o
--- tutorial.cpp
+++ tutorial.cpp
@@ -1,28 +1,53 @@
 // Following the steps from https://vulkan-tutorial.com/
 
-#include <vulkan/vulkan.h>
+//#include <vulkan/vulkan.h>
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
 
 #include <iostream>
 #include <stdexcept>
 #include <cstdlib>
 
+const uint32_t WINDOW_WIDTH = 800;
+const uint32_t WINDOW_HEIGHT = 600;
+const char* WINDOW_TITLE = "Hello Triangle";
+
 class HelloTriangleApplication {
 public:
 	void run() {
+		initWindow();
 		initVulkan();
 		mainLoop();
 		cleanup();
 	}
 
 private:
+	void initWindow() {
+		glfwInit();
+
+		glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);  // do not create the default OpenGL context
+		glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);  // will relax this requirement later
+
+		window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE, nullptr, nullptr);
+	}
+
 	void initVulkan() {
 	}
 
 	void mainLoop() {
+		while (!glfwWindowShouldClose(window)) {
+			glfwPollEvents();
+		}
 	}
 
 	void cleanup() {
+		glfwDestroyWindow(window);
+
+		glfwTerminate();
 	}
+
+private:
+	GLFWwindow* window;
 };
 
 int main() {
